2acde432606ffdfd7e6cc05c0c80fe929a4d85c1
toancyto09 <101265301+toancyto09@users.noreply.github.com>
2023-08-18 12:26:50 +0700

    create sussces

--- src/routes/blog.js
+++ src/routes/blog.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const blogController = require('../app/controllers/BlogController');
 
+router.use('/create', blogController.create);
+router.use('/store', blogController.store);
 router.use('/:slug', blogController.Show);
 
 module.exports = router;
